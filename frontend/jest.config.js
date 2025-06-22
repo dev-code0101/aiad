5ca8d4c5ad96201a5e1807e5c648ee33e837b0b0
dev-code0101 <dev.codeone@proton.me>
2025-06-22 20:50:56 +0530

    dev: removed jest transform option

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,9 +3,9 @@ const config = {
   verbose: true,
   //   testRunner: "<rootDir>/node_modules/jest-circus/build/runner.js",
   testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
+  //   transform: {
+  //     "^.+\\.jsx?$": "babel-jest",
+  //   },
 };
 
 module.exports = config;
